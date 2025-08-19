a76afbd30325cfecbebddbb306719af458ae2870
vasudevvs311-arch <vasudevvs311@gmail.com>
2025-08-19 22:37:58 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'GHOST-MD;',
+  author: 'GHOST-MD',
+  botName: "GHOST-MD",
+  botOwner: 'GHOST-MD', // Your name
+  ownerNumber: '918301966072', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
